a7d4f3eb6f8660544ac30defa49f4c28ecc39fd9
FahimSyedNY <fahims13@nycstudents.net>
2026-06-02 01:21:23 -0400

    Added Dynamic Camera, Flipping player, Running Anim, and Background up down parallax.

--- core/src/main/java/entities/Player.java
+++ core/src/main/java/entities/Player.java
@@ -1,7 +1,6 @@
 package entities;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
